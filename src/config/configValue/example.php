d5d967fbe5702e30270936721a8245a39a38047f
ehrlichandreas <ehrlichandreas@users.noreply.github.com>
2014-08-18 11:09:14 +0200

    Update example.php

--- src/config/configValue/example.php
+++ src/config/configValue/example.php
@@ -4,39 +4,39 @@ return (object) array(
 	'db' => (object) array(
 		'host' => 'localhost',
 		'port' => '3306',
-		'name' => 'dobild',
-		'username' => 'root',
-		'password' => 'root',
+		'name' => '',
+		'username' => '',
+		'password' => '',
 	),
 	'mail' => (object) array(
 		'main' => (object) array(
 			'from' => (object) array(
-				'email' => 'main@dobild.de',
-				'name'  => 'doBild',
+				'email' => 'info@xxxxx.de',
+				'name'  => 'xxxxx',
 			),
 			'replyto' => (object) array(
-				'email' => 'main@dobild.de',
-				'name'  => 'doBild',
+				'email' => 'info@xxxxx.de',
+				'name'  => 'xxxxx',
 			),
 		),
 		'contact'   => (object) array(
 			'from' => (object) array(
-				'email' => 'contact@dobild.de',
-				'name'  => 'doBild',
+				'email' => 'info@xxxxx.de',
+				'name'  => 'xxxxx',
 			),
 			'replyto' => (object) array(
-				'email' => 'contact@dobild.de',
-				'name'  => 'doBild',
+				'email' => 'info@xxxxx.de',
+				'name'  => 'xxxxx',
 			),
 		),
 		'transport' => (object) array(
 			'smtp' => true,
-			'host' => 'mail.dn21.de',
-			'port' => '25',
-			'auth' => 'login',
-			'username' => 'webapp@epsservice.org',
-			'password' => 'r1s94z45',
-			'name' => 'mail.dn21.de',
+			'host' => '',
+			'port' => '',
+			'auth' => '',
+			'username' => '',
+			'password' => '',
+			'name' => '',
 		),
 	),
 );
